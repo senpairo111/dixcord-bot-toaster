800095b892371995155f6be5b57f5c281ea1f369
senpairo111 <74233627+senpairo111@users.noreply.github.com>
2020-12-10 10:20:39 +0200

    Add files via upload

--- dixcord bot/scripts/commands.py
+++ dixcord bot/scripts/commands.py
@@ -20,13 +20,8 @@ class Handler(commands.Cog):
 
     @commands.command()
     async def when_is_p5s_getting_a_western_release(self, ctx):
-        await ctx.send('...')
-        await ctx.send('...')
-        await ctx.send('...')
-        await ctx.send('...')
-        await ctx.send('...')
-        await ctx.send('...')
-        await ctx.send('good question')
+        await ctx.send('23.2.2021 LETS FUCKING GOOOOOOOOOOOOO!!!!!!!!!!!!!!')
+        
 
     @commands.command()
     async def explain(self, ctx):
@@ -51,16 +46,39 @@ class Handler(commands.Cog):
     
     @commands.command()
     async def send_nudes(self, ctx):
-        pics_array = ['https://cdn.discordapp.com/attachments/653960437112635406/774256227369877514/20201106_145732.jpg', 'https://cdn.discordapp.com/attachments/773997385717252129/775348868446552064/Z.png', 'https://cdn.discordapp.com/attachments/663798504682029096/775349600785530891/Screenshot_20200211-192727_TikTok.jpg', 'https://cdn.discordapp.com/attachments/663798504682029096/775349845778366474/Screenshot_20201101-085510_Instagram.jpg', 'https://cdn.discordapp.com/attachments/663798504682029096/775349916515565578/Screenshot_20201011-215015_Instagram.jpg', 'https://cdn.discordapp.com/attachments/663798504682029096/775349997146472448/Screenshot_20201019-171217_Instagram.jpg', 'https://cdn.discordapp.com/attachments/773538182683557889/774388789098577940/20201106_234408.jpg', 'https://cdn.discordapp.com/attachments/773538182683557889/773937924403888168/sketch-1604327984843.jpg', 'https://cdn.discordapp.com/attachments/653933938427035650/756979780347232286/luigi.jpg', 'https://cdn.discordapp.com/attachments/707163826369331253/774943954675826738/MORGANA_TRANSFORM.gif', 'https://cdn.discordapp.com/attachments/653960437112635406/775352768008945684/DA_G-MAN.jpg', 'https://cdn.discordapp.com/attachments/653960437112635406/775352774665568256/donkey_kong_cosplay.jpg', 'https://cdn.discordapp.com/attachments/653960437112635406/775352775831715860/donkey_kong_2.jpg',  'https://cdn.discordapp.com/attachments/653960437112635406/775352776145502208/donkey_kong.jpg', 'https://cdn.discordapp.com/attachments/653960437112635406/775352781053231124/donkey_kong.png', 'https://cdn.discordapp.com/attachments/653960437112635406/775352816390373376/elmo_on_fire.gif', 'https://cdn.discordapp.com/attachments/653960437112635406/775352843414536232/dancing_spider-man.gif', 'https://media.tenor.com/images/8566d06f18ee38247a28719d9aa55355/tenor.gif','https://media.tenor.com/images/d0f644525d3be9eb3b287edca97d20f4/tenor.gif','https://media.tenor.com/images/01eb0b47e0919f8b02407010a08960dd/tenor.gif']
+        pics_array = ['https://cdn.discordapp.com/attachments/653960437112635406/774256227369877514/20201106_145732.jpg', 'https://cdn.discordapp.com/attachments/773997385717252129/775348868446552064/Z.png', 'https://cdn.discordapp.com/attachments/663798504682029096/775349600785530891/Screenshot_20200211-192727_TikTok.jpg', 'https://cdn.discordapp.com/attachments/663798504682029096/775349845778366474/Screenshot_20201101-085510_Instagram.jpg', 'https://cdn.discordapp.com/attachments/663798504682029096/775349916515565578/Screenshot_20201011-215015_Instagram.jpg', 'https://cdn.discordapp.com/attachments/663798504682029096/775349997146472448/Screenshot_20201019-171217_Instagram.jpg', 'https://cdn.discordapp.com/attachments/773538182683557889/774388789098577940/20201106_234408.jpg', 'https://cdn.discordapp.com/attachments/773538182683557889/773937924403888168/sketch-1604327984843.jpg', 'https://cdn.discordapp.com/attachments/653933938427035650/756979780347232286/luigi.jpg', 'https://cdn.discordapp.com/attachments/707163826369331253/774943954675826738/MORGANA_TRANSFORM.gif', 'https://cdn.discordapp.com/attachments/653960437112635406/775352768008945684/DA_G-MAN.jpg', 'https://cdn.discordapp.com/attachments/653960437112635406/775352774665568256/donkey_kong_cosplay.jpg', 'https://cdn.discordapp.com/attachments/653960437112635406/775352775831715860/donkey_kong_2.jpg',  'https://cdn.discordapp.com/attachments/653960437112635406/775352776145502208/donkey_kong.jpg', 'https://cdn.discordapp.com/attachments/653960437112635406/775352781053231124/donkey_kong.png', 'https://cdn.discordapp.com/attachments/653960437112635406/775352816390373376/elmo_on_fire.gif', 'https://cdn.discordapp.com/attachments/653960437112635406/775352843414536232/dancing_spider-man.gif', 'https://media.tenor.com/images/8566d06f18ee38247a28719d9aa55355/tenor.gif','https://media.tenor.com/images/d0f644525d3be9eb3b287edca97d20f4/tenor.gif','https://media.tenor.com/images/01eb0b47e0919f8b02407010a08960dd/tenor.gif', 'https://media.discordapp.net/attachments/692983723125243916/783787970398257262/IMG-20201201-WA0008.jpg', 'https://i.pinimg.com/originals/2c/fc/fa/2cfcfabf5f5fe0aa109cbe34aa12df2a.gif', 'https://tenor.com/2id6.gif', 'https://tenor.com/bbJ2N.gif', 'https://tenor.com/boi67.gif', 'https://tenor.com/2XQa.gif', 'https://tenor.com/bpSmL.gif', 'https://tenor.com/bn6FR.gif', 'https://tenor.com/bsfeC.gif', 'https://tenor.com/bmX4A.gif', 'https://tenor.com/bmPld.gif', 'https://tenor.com/R4Ah.gif']
         await ctx.send(random.choice(pics_array))
 
     @commands.command()
     async def send_zoom(self, ctx, the_class=""):
-        if the_class == "lesson name":
-            await ctx.send('lesson link')
-        elif the_class == "lesson 2 name":
-            await ctx.send('lesson 2 link')
-        #etc
+        if the_class == "ערבית":
+            await ctx.send('https://edu-il.zoom.us/j/7328072750')
+        elif the_class == "כישורי חיים":
+            await ctx.send('https://edu-il.zoom.us/j/96050052341?pwd=RndxY0M4RElXQzRpV2dXeGpXdVdaQT09')
+        elif the_class == "ספורט" or the_class == "חנג":
+            await ctx.send('https://us04web.zoom.us/j/75667411544?pwd=bm1jZ1F0b0syVnZJSjduYUtOc1h0dz09')
+        elif the_class == "חינוך":
+            await ctx.send('https://edu-il.zoom.us/j/92207525681?pwd=czJqblZpSzZkaGREZVVmVFV6UHlYdz09')
+        elif the_class == "מדעים":
+            await ctx.send('https://edu-il.zoom.us/j/92439764649?pwd=Q2NkNWVKdmtiVVB2VllPNG5vdVdlZz09')
+        elif the_class == "מתמטיכיף":
+            await ctx.send('https://edu-il.zoom.us/j/94422187142?pwd=MUJmRG8xR0w4WitiWVVHYmNZaWlBdz09')
+        elif the_class == "מתמטיקה":
+            await ctx.send('https://edu-il.zoom.us/j/6029618489?pwd=VlZidW84Z3E2L3RCK2hKWWg1ZzRUQT09')
+        elif the_class == "ספרות":
+            await ctx.send('https://edu-il.zoom.us/j/3557883542')
+        elif the_class == "שלח":
+            await ctx.send('https://edu-il.zoom.us/j/6044478281')
+        elif the_class == "אזרחות":
+            await ctx.send('https://edu-il.zoom.us/j/93395805154?pwd=dm0vY2ozc3pOUWV0MGdlazlaTHJSUT09')
+        elif the_class == "אנגלית":
+            await ctx.send('https://us04web.zoom.us/j/72069513802?pwd=NEs1Ym1xbnRBS0U4ZWNsaXFzbm1Ndz09')
+        elif the_class == "עברית":
+            await ctx.send('https://edu-il.zoom.us/j/93926817554?pwd=YldIRWUwbmhOTlFNdTdsZkZzUmRZQT09')
+        elif the_class == "תקשורת":
+            await ctx.send('https://edu-il.zoom.us/j/92207525681?pwd=czJqblZpSzZkaGREZVVmVFV6UHlYdz09')
+        elif the_class == "היסטוריה":
+            await ctx.send('https://edu-il.zoom.us/j/96656013660?pwd=Um5BeE9CRktySm9UbWNPUDErcitKQT09')
         else:
             await ctx.send('class not found, the avalibe classes are: ערבית, כישורי חיים, ספורט, חינוך, מדעים, מתמטיכיף, מתמטיקה, ספרות, שלח, אזרחות, אנגלית, עברית, תקשורת, היסטוריה')
     # @client.command()
